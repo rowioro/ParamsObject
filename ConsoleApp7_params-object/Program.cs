565facb16ff8e29aaacc1d651fe0d32b2ec734da
Robert Stefaniak <01138659@pw.edu.pl>
2020-06-01 18:53:39 +0200

    Add project files.

---
+++ ConsoleApp7_params-object/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7_params_object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Zamiana("Mam na imie #0 i mam #1 lat.", "Robert", 48));
+            Console.ReadKey();
+        }
+
+        public static string Zamiana(string tekst, params object[]args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                tekst = tekst.Replace("#" + i, args[i].ToString());
+            }
+            return tekst;
+        }
+    }
+}
